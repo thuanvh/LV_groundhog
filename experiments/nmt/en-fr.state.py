6c2492c4de75023abad478743bf9b24143587e56
thuan <thuanvh@gmail.com>
2016-10-09 16:54:54 +0700

    Modify of some import errors.

---
+++ experiments/nmt/en-fr.state.py
@@ -0,0 +1,14 @@
+dict(
+    # Source sequences
+    source = ["/home/thuan/sandbox/nlp/largevocab/LV_groundhog/experiments/nmt/binarized_text.en.h5"],
+    # Target sequences
+    target = ["/home/thuan/sandbox/nlp/largevocab/LV_groundhog/experiments/nmt/binarized_text.fr.h5"],
+    # index -> word dict for the source language,
+    indx_word = "/home/thuan/sandbox/nlp/largevocab/LV_groundhog/experiments/nmt/ivocab.en.pkl",
+    # index -> word dict for the target language
+    indx_word_target = "/home/thuan/sandbox/nlp/largevocab/LV_groundhog/experiments/nmt/ivocab.fr.pkl",
+    # word -> index dict for the source language
+    word_indx = "/home/thuan/sandbox/nlp/largevocab/LV_groundhog/experiments/nmt/vocab.en.pkl",
+    # word -> index dict for the target language
+    word_indx_trgt = "/home/thuan/sandbox/nlp/largevocab/LV_groundhog/experiments/nmt/vocab.fr.pkl"
+)
